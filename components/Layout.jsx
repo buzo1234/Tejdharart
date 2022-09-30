0839b5bed2fec90357f523121cb2dd92ce6d4693
buzo1234 <karandua2002@gmail.com>
2022-09-30 19:23:03 +0530

    bug_fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -15,7 +15,7 @@ const Layout = ({ children }) => {
       {modal ? (
         <>
           <div className='w-screen flex fixed justify-center items-center my-auto z-20 h-screen bg-gray-700/50'>
-            <div className="transition delay-200 translate-y-0 flex-col ease-in-out w-3/4 h-fit px-3 py-1 mx-auto flex bg-[url('/Background.jpg')] rounded-xl shadow-2xl">
+            <div className="transition delay-200 translate-y-0 flex-col ease-in-out w-11/12 md:w-3/4 lg:w-3/4 xl:w-3/4 h-3/4 px-2 md:px-3 lg:px-3 xl:px-3 py-1 mx-1 flex bg-[url('/Background.jpg')] rounded-xl shadow-2xl">
               <button
                 onClick={() => setModal(false)}
                 className='flex w-full justify-end text-red-600 text-xl font-bold'
@@ -29,7 +29,7 @@ const Layout = ({ children }) => {
       ) : (
         <>
           <div className='w-screen fixed justify-center items-center my-auto z-20 h-screen bg-gray-700/50 transition delay-200 -translate-y-full ease-in-out '>
-            <div className=" flex-col w-3/4 h-fit px-3 py-1 mx-auto flex bg-[url('/Background.jpg')] rounded-xl shadow-2xl">
+            <div className=" flex-col  w-11/12 md:w-3/4 lg:w-3/4 xl:w-3/4 h-3/4 px-2 md:px-3 lg:px-3 xl:px-3 py-1 mx-auto flex bg-[url('/Background.jpg')] rounded-xl shadow-2xl">
               <button
                 onClick={() => setModal(false)}
                 className='flex w-full justify-end text-red-600 text-xl font-bold'
