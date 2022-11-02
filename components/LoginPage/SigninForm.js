b605c074295098b29548e0e20b430e9b47ba492c
buzo1234 <karandua2002@gmail.com>
2022-11-02 05:37:57 +0530

    d1

--- components/LoginPage/SigninForm.js
+++ components/LoginPage/SigninForm.js
@@ -29,7 +29,7 @@ const SigninForm = ({ isAnimated, setIsAnimated }) => {
           Accept: 'application/json',
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+          'Access-Control-Allow-Methods': 'DELETE, POST, GET',
           'Access-Control-Allow-Headers':
             'Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization',
         },
