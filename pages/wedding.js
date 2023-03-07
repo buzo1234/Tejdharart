4990dfd320cfb2c33c7bd3878bd036816770cf65
buzo1234 <karandua2002@gmail.com>
2023-03-07 17:05:41 +0530

    image decoration

--- pages/wedding.js
+++ pages/wedding.js
@@ -4,6 +4,8 @@ import Head from 'next/head';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
+import ReactImageMagnify from 'react-image-magnify';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 const Wedding = () => {
   const { cat } = useStateContext();
@@ -11,8 +13,13 @@ const Wedding = () => {
   const [emailForm, setEmailForm] = useState('');
   const [phoneForm, setPhoneForm] = useState();
   const [descForm, setDescForm] = useState('');
+  const [index, setIndex] = useState(0);
+
 
   const [flag, setFlag] = useState(false);
+
+  const productImage = ['/wg1.jpeg', '/wg2.jpeg', '/wg3.jpeg'];
+
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -90,59 +97,130 @@ const Wedding = () => {
         <br />
         <br />
 
-        <p className='font-bold my-3 text-lg'>
-          Please fill this form for further enquiries.
-        </p>
-        <form className='flex flex-col w-full md:w-3/5 lg:w-3/5 xl:w-3/5'>
-          <input
-            onChange={(e) => setNameForm(e.target.value)}
-            value={nameForm}
-            type='text'
-            className='mb-4 border-[1px] border-gray-600 border-solid px-3 py-2'
-            placeholder='Enter your name'
-          />
-          <input
-            onChange={(e) => setEmailForm(e.target.value)}
-            value={emailForm}
-            type='email'
-            className='mb-4 border-[1px] border-gray-600 border-solid px-3 py-2'
-            placeholder='Enter your email'
-          />
-          <div className='flex w-full '>
-            <input
-              onChange={(e) => setPhoneForm(e.target.value)}
-              value={phoneForm}
-              type='number'
-              placeholder='Enter your phone number'
-              className='mb-4 w-full border-[1px] border-gray-600 border-solid px-3 py-2'
-            />
-          </div>
-          <textarea
-            onChange={(e) => setDescForm(e.target.value)}
-            value={descForm}
-            className=' border-[1px] border-gray-600 border-solid px-3 py-2'
-            name='description'
-            id='description'
-            placeholder='Please describe your request'
-            cols='30'
-            rows='6'
-          ></textarea>
-          <div className='buttons'>
-            <button
-              type='button'
-              className='add-to-cart'
-              onClick={(e) => handleFormSubmit(e)}
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+        <div className='product-detail-container grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 items-center'>
+          <div className='col-span-1 md:col-span-2 lg:col-span-2 xl:col-span-2 '>
+            <p className='font-bold my-3 text-lg'>
+              Please fill this form for further enquiries.
+            </p>
+            <form className='flex flex-col w-full md:w-3/5 lg:w-3/5 xl:w-3/5'>
+              <input
+                onChange={(e) => setNameForm(e.target.value)}
+                value={nameForm}
+                type='text'
+                className='mb-4 border-[1px] border-gray-600 border-solid px-3 py-2'
+                placeholder='Enter your name'
+              />
+              <input
+                onChange={(e) => setEmailForm(e.target.value)}
+                value={emailForm}
+                type='email'
+                className='mb-4 border-[1px] border-gray-600 border-solid px-3 py-2'
+                placeholder='Enter your email'
+              />
+              <div className='flex w-full '>
+                <input
+                  onChange={(e) => setPhoneForm(e.target.value)}
+                  value={phoneForm}
+                  type='number'
+                  placeholder='Enter your phone number'
+                  className='mb-4 w-full border-[1px] border-gray-600 border-solid px-3 py-2'
+                />
+              </div>
+              <textarea
+                onChange={(e) => setDescForm(e.target.value)}
+                value={descForm}
+                className=' border-[1px] border-gray-600 border-solid px-3 py-2'
+                name='description'
+                id='description'
+                placeholder='Please describe your request'
+                cols='30'
+                rows='6'
+              ></textarea>
+              <div className='buttons'>
+                <button
+                  type='button'
+                  className='add-to-cart'
+                  onClick={(e) => handleFormSubmit(e)}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
 
-        {flag ? (
-          <p className='text-lg text-green-800 font-semibold md:text-xl lg:text-xl xl:text-xl'>
-            Message sent! You will hear from us shortly
-          </p>
-        ) : null}
+            {flag ? (
+              <p className='text-lg text-green-800 font-semibold md:text-xl lg:text-xl xl:text-xl'>
+                Message sent! You will hear from us shortly
+              </p>
+            ) : null}
+          </div>
+          <div className='col-span-1  flex flex-col w-full'>
+            <div className='image-container flex relative'>
+              {productImage ? (
+                <>
+                  <div
+                    className='absolute top-0 left-0 flex h-full mr-1 items-center justify-center cursor-pointer z-40 rounded-tl-lg rounded-bl-lg w-[50px]  text-6xl '
+                    onClick={() =>
+                      setIndex(
+                        index === 0 ? productImage.length - 1 : index - 1
+                      )
+                    }
+                  >
+                    <FiChevronLeft className='px-2 py-2 bg-white bg-opacity-20 font-bold text-black' />
+                  </div>
+                  <ReactImageMagnify
+                    {...{
+                      smallImage: {
+                        isFluidWidth: true,
+                        src: productImage[index],
+                      },
+                      largeImage: {
+                        src: productImage[index],
+                        height: 1500,
+                        width: 1500,
+                      },
+                      /* enlargedImagePosition: 'over', */
+                      imageClassName: 'product-detail-image object-contain',
+                      enlargedImageClassName: 'max-w-[1500px] object-contain',
+
+                      enlargedImageContainerClassName: 'bg-red-500 z-30',
+                      isHintEnabled: true,
+                    }}
+                  />
+                  <div
+                    className='absolute top-0 right-0 flex h-full ml-1 items-center justify-center cursor-pointer z-40 rounded-tr-lg rounded-br-lg w-[50px]  text-6xl '
+                    onClick={() =>
+                      setIndex(
+                        index !== productImage.length - 1 ? index + 1 : 0
+                      )
+                    }
+                  >
+                    <FiChevronRight className='px-2 py-2 bg-white bg-opacity-20 font-bold text-black' />
+                  </div>
+                </>
+              ) : /* <img
+            src={
+              productImage && urlFor(productImage && productImage[index])
+            }
+                className='product-detail-image object-contain'
+                />  */
+              null}
+            </div>
+            <div className='small-images-container'>
+              {productImage?.map((item, i) => (
+                <img
+                  key={i}
+                  src={item}
+                  className={
+                    i === index
+                      ? 'small-image selected-image border-2 border-solid border-red-500 border-spacing-2'
+                      : 'small-image'
+                  }
+                  onMouseEnter={() => setIndex(i)}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
 
       <Footer data={cat} />
